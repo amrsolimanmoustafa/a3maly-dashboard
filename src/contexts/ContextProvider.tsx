a874ff475782d0c70dc2ce1c04fa99b0bb639adc
Mohamed Tarek <m.tarekazab1997@gmail.com>
2023-08-12 23:16:33 +0300

    context provider effect

--- src/contexts/ContextProvider.tsx
+++ src/contexts/ContextProvider.tsx
@@ -1,33 +1,17 @@
 import { AuthProvider } from "./auth-context";
-import ClientContextProvider from "./client-context";
 import ContactMessageContextProvider from "./contact-message-context";
-import DriverContextProvider from "./driver-context";
-import OrderContextProvider from "./order-context";
-import VehicleContextProvider from "./vehicle-context";
-import OfficeContextProvider from "./office-context";
 import FinancialsProvider from '@/contexts/financials-context';
-import DestinationsContextProvider from '@/contexts/destinations-context';
 import AdminContextProvider from '@/contexts/admin-context';
 
 const ContextProvider = ({ children }: any) => {
   return (
-      <DriverContextProvider>
-        <ContactMessageContextProvider>
-          <OrderContextProvider>
-            <FinancialsProvider>
-              <VehicleContextProvider>
-                <OfficeContextProvider>
-                  <DestinationsContextProvider>
-                    <AdminContextProvider>
-                      <AuthProvider>{children}</AuthProvider>
-                    </AdminContextProvider>
-                  </DestinationsContextProvider>
-                </OfficeContextProvider>
-              </VehicleContextProvider>
-            </FinancialsProvider>
-          </OrderContextProvider>
-        </ContactMessageContextProvider>
-      </DriverContextProvider>
+    <ContactMessageContextProvider>
+      <FinancialsProvider>
+        <AdminContextProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </AdminContextProvider>
+      </FinancialsProvider>
+    </ContactMessageContextProvider>
   );
 };
 export default ContextProvider;
