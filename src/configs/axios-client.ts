bff6a371384da0d32299c600741fb09df59ce2cb
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-10-03 15:46:14 +0300

    feat: swap api with .env value

--- src/configs/axios-client.ts
+++ src/configs/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://194.163.137.50/admin",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
 });
 
 axiosClient.defaults.headers.common = {
